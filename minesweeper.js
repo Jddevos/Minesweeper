75aa7dbec27f55fec32b5778d5f1ecbb8f4bb23d
Jeramy De Vos <Jddevos@users.noreply.github.com>
2019-04-24 13:14:53 -0500

    Initial commit

---
+++ minesweeper.js
@@ -0,0 +1,315 @@
+var board = new Array();	//This will hold the game board in its entirety
+var toCheck = new Array();	//Will hold the list of cells to check when expanding empty space
+var totalRows;
+var totalCols;
+var totalMines;
+
+document.oncontextmenu=function() {
+	//This disables the right click context menu
+	return false;
+}
+
+function checkWin() {
+	var unpressedCells = 0;
+
+	for (var i=0; i<totalRows; i++) {
+		for (var j=0; j<totalCols; j++) {
+			var cur = document.getElementById('btn_'+i+'_'+j);
+			if (!cur.classList.contains('pressed') && !cur.classList.contains('exploded')) {
+				//We dont want to count pressed or exploded cells
+				unpressedCells ++;
+			}
+			else if (cur.value == 'F') {
+				//This is unnecessary, as flagged cells will not be pressed. Including for completionism
+				unpressedCells ++;
+			}
+		}
+	}
+
+	// console.log(unpressedCells+ ' unpressed cells remaining');
+
+	if (unpressedCells == totalMines && document.querySelector('.exploded') == null) {
+		pauseTimer();
+
+		disableBoard();
+		displayMines();
+		updateFlagCount();
+
+		window.alert('You won!');
+	}
+}
+
+function disableBoard() {
+	for (var i=0; i<totalRows; i++) {
+		for (var j=0; j<totalCols; j++) {
+			var curBtn = document.getElementById('btn_'+i+'_'+j);
+
+			//Disable the button
+			curBtn.disabled=true;
+		}
+	}
+}
+
+function displayMines() {
+	for (var i=0; i<totalRows; i++) {
+		for (var j=0; j<totalCols; j++) {
+			var cur = document.getElementById('btn_'+i+'_'+j);
+			if (!cur.classList.contains('pressed') && cur.value=="" && board[i][j].mine) {
+				cur.value = "*";
+				cur.classList.add('textB');
+			}
+		}
+	}
+}
+
+function expandEmptySpace() {
+	r=parseInt(toCheck[0].row,10);
+	c=parseInt(toCheck[0].col,10);
+
+	// console.log('r: ' + r);
+	// console.log('c: ' + c);	
+
+	//Dequeue the first element
+	toCheck.shift();
+
+	//Set the cell to checked
+	board[r][c].checked = true;
+
+
+	//Add the class
+	document.getElementById('btn_'+r+'_'+c).className += " pressed";
+
+	//Display the value
+	if (parseInt(board[r][c].value) > 0) {
+		document.getElementById('btn_'+r+'_'+c).value = board[r][c].value;
+		document.getElementById('btn_'+r+'_'+c).className += " text"+board[r][c].value;
+	}
+
+	if (board[r][c].value == 0) {
+		//Reset the value, just in case a flag exists
+		document.getElementById('btn_'+r+'_'+c).value = "";
+
+		//We want to check all of the neighbors as well
+		if (!isUndefined(board, r+1, c  ) && !board[r+1][c  ].checked) { toCheck.push({row: r+1, col: c  }); board[r+1][c  ].checked=true; }
+		if (!isUndefined(board, r-1, c  ) && !board[r-1][c  ].checked) { toCheck.push({row: r-1, col: c  }); board[r-1][c  ].checked=true; }
+		if (!isUndefined(board, r  , c+1) && !board[r  ][c+1].checked) { toCheck.push({row: r  , col: c+1}); board[r  ][c+1].checked=true; }
+		if (!isUndefined(board, r  , c-1) && !board[r  ][c-1].checked) { toCheck.push({row: r  , col: c-1}); board[r  ][c-1].checked=true; }
+		if (!isUndefined(board, r+1, c+1) && !board[r+1][c+1].checked) { toCheck.push({row: r+1, col: c+1}); board[r+1][c+1].checked=true; }
+		if (!isUndefined(board, r+1, c-1) && !board[r+1][c-1].checked) { toCheck.push({row: r+1, col: c-1}); board[r+1][c-1].checked=true; }
+		if (!isUndefined(board, r-1, c+1) && !board[r-1][c+1].checked) { toCheck.push({row: r-1, col: c+1}); board[r-1][c+1].checked=true; }
+		if (!isUndefined(board, r-1, c-1) && !board[r-1][c-1].checked) { toCheck.push({row: r-1, col: c-1}); board[r-1][c-1].checked=true; }
+	}
+	// console.log('Elements left to check: ' +toCheck.length);
+	// console.log(toCheck[0].row + ' ' + toCheck[0].col);
+}
+
+function generateBoard() {
+	//Clear the board
+	board = new Array();
+
+	for (var i=0; i<totalRows; i++) {
+		var row = new Array();
+
+		for (var j=0; j<totalCols; j++) {
+			var cell = {value: "0", mine: false, checked: false};
+			row.push(cell);
+		}
+		board.push(row);
+	}
+
+	for (var i=0; i<totalMines; i++) {
+		//get random row and column
+		do  {
+			r = Math.floor(Math.random() * totalRows)
+			c = Math.floor(Math.random() * totalCols)
+			// console.log("Checking " +r+ ", " +c);
+		} while (board[r][c].mine==true)
+
+		// console.log("Placing mine at " +r+ ", " +c)
+		board[r][c].mine = true;
+		board[r][c].value = "*";
+
+		//increment the cells around the mine
+		if (!isUndefined(board, r+1, c  )) {board[r+1][c  ].value++;}
+		if (!isUndefined(board, r-1, c  )) {board[r-1][c  ].value++;}
+		if (!isUndefined(board, r  , c+1)) {board[r  ][c+1].value++;}
+		if (!isUndefined(board, r  , c-1)) {board[r  ][c-1].value++;}
+		if (!isUndefined(board, r+1, c+1)) {board[r+1][c+1].value++;}
+		if (!isUndefined(board, r+1, c-1)) {board[r+1][c-1].value++;}
+		if (!isUndefined(board, r-1, c+1)) {board[r-1][c+1].value++;}
+		if (!isUndefined(board, r-1, c-1)) {board[r-1][c-1].value++;}
+	}
+
+	// console.log(board);
+}
+
+function generateDisplay() {
+	boardDiv = document.getElementById("board");
+	boardDiv.innerHTML = "";
+	
+	//Generate cells
+	for (var i=0; i<totalRows; i++) {
+		var rowDiv = document.createElement('div');
+		rowDiv.className = "gameBoardRow";
+
+		for (var j=0; j<totalCols; j++) {
+			rowDiv.innerHTML += "<input type='button' class='gameBoardBtn' id='btn_"+i+"_"+j+"' row='"+i+"' col='"+j+"' onclick='play(this, event)' oncontextmenu='play(this, event)'>"
+		}
+		boardDiv.appendChild(rowDiv);
+	}
+
+	//Generate info panel
+	infoDiv = document.createElement('div');
+	timerSpan = document.createElement('span');
+	flagsSpan = document.createElement('span');
+
+	infoDiv.id = "infoContainer";
+	timerSpan.className = "timer";
+	timerSpan.id = "timer";
+	flagsSpan.className = "remainingFlags";
+	flagsSpan.id = "remainingFlags";
+
+	infoDiv.appendChild(timerSpan);
+	infoDiv.appendChild(flagsSpan);
+
+	//Append to boardDiv
+	boardDiv.appendChild(infoDiv);
+}
+
+function isUndefined(_arr, _index1, _index2) {
+    try {
+    	return _arr[_index1][_index2] == undefined;
+    } catch(e)
+    {
+    	return true;
+    }
+}
+
+function isValid() {
+	//Check validity of input
+	var valid = true;
+	var alertPanel = document.getElementById('alertPanel').innerHTML;
+
+	alertPanel = "";	
+
+	if (totalRows < 5 || totalRows > 35) {
+		valid = false;
+	}
+	else if (totalCols < 5 || totalCols > 75) {
+		valid = false;
+	}
+	else if (totalMines < 0 || totalMines > totalRows * totalCols) {
+		valid = false;
+	}
+
+	return valid;
+}
+
+function loseEndGame() {
+	pauseTimer();
+	for (var i=0; i<totalRows; i++) {
+		for (var j=0; j<totalCols; j++) {
+			var curBtn = document.getElementById('btn_'+i+'_'+j);
+			if (board[i][j].mine) {
+				curBtn.value = "*";
+				curBtn.className += " exploded";
+			}
+		}
+	}
+	disableBoard();
+}
+
+function play(ele, event) {
+	startTimer(); //Start the timer, if it isnt already
+	var clickedRow = ele.getAttribute('row');
+	var clickedCol = ele.getAttribute('col')
+	var clickedBtn = document.getElementById('btn_'+clickedRow+'_'+clickedCol);
+	// console.log('Value: ' +board[clickedRow][clickedCol].value);
+
+	if (event.type == 'click') {
+		//Left click
+		// console.log('Left click at '+clickedRow+", "+clickedCol);
+
+		//Check for Lose
+		if (board[clickedRow][clickedCol].mine) {
+			clickedBtn.className += " text5";
+			loseEndGame();
+		}
+
+		//Check if blank
+		if (board[clickedRow][clickedCol].value == 0) {
+			toCheck.push({row: clickedRow, col: clickedCol});
+			//We want to set this button to pressed, and expand until we hit the edge or numbers
+			while (toCheck.length>0) {
+				expandEmptySpace();
+			}
+		}
+
+		//Check if number
+		if (board[clickedRow][clickedCol].value > 0) {
+			clickedBtn.className += " pressed text"+board[clickedRow][clickedCol].value;
+			clickedBtn.value = board[clickedRow][clickedCol].value;
+			board[clickedRow][clickedCol].visited = true;
+		}
+	}
+	if (event.type == 'contextmenu') {
+		//Right click
+		// console.log('Right click at '+clickedRow+", "+clickedCol);
+
+		//Switch through empty, F, and ?
+		// clickedBtn.value = clickedBtn.value == "" ? "F" : clickedBtn.value == "F" ? "?" : "";
+
+		if (!clickedBtn.classList.contains('pressed')) {
+			if (clickedBtn.value == "") {
+				clickedBtn.value = "F";
+				clickedBtn.className = "gameBoardBtn textF"
+			}
+			else if (clickedBtn.value == "F") {
+				clickedBtn.value = "?";
+				clickedBtn.className = "gameBoardBtn textQ"
+			}
+			else if (clickedBtn.value == "?") {
+				clickedBtn.value = "";
+				clickedBtn.className = "gameBoardBtn"
+			}
+		}	
+	}
+
+	updateFlagCount();
+	
+	//Check for win
+	checkWin();
+}
+
+function start() {
+	//pull in generation information
+	totalRows = document.getElementById("rows").value;
+	totalCols = document.getElementById("cols").value;
+	totalMines = document.getElementById("mines").value;
+
+	document.getElementById("mines").max = totalRows * totalCols;
+
+	if (isValid()) {
+		flagsLeft = totalMines;
+
+		generateBoard();
+		generateDisplay();
+		resetTimer();
+	}
+}
+
+function updateFlagCount() {
+	var foundFlags=0;
+
+	for (var i=0; i<totalRows; i++) {
+		for (var j=0; j<totalCols; j++) {
+			var c = document.getElementById('btn_'+i+'_'+j);
+			if (c.value=='F' || (c.value=='*' && !c.classList.contains('exploded')))
+				foundFlags++;
+		}
+	}
+
+	var flagsLeft = totalMines-foundFlags;
+
+	document.getElementById('remainingFlags').innerHTML = flagsLeft<0 ? 0 : flagsLeft;
+}
---
+++ timer.js
@@ -0,0 +1,59 @@
+var timerDisplay;
+var startTime;
+var updatedTime;
+var difference;
+var tInterval;
+var savedTime;
+var paused = 0;
+var running = 0;
+
+function startTimer(){
+  if(!running){
+	startTime = new Date().getTime();
+	tInterval = setInterval(getShowTime, 1);
+	// change 1 to 1000 above to run script every second instead of every millisecond. one other change will be needed in the getShowTime() function below for this to work. see comment there.   
+ 
+	paused = 0;
+	running = 1;
+  }
+}
+
+function pauseTimer(){
+	clearInterval(tInterval);
+	savedTime = difference;
+	paused = 1;
+	running = 0;
+}
+
+function resetTimer(){
+	timerDisplay = document.getElementById('timer');
+	clearInterval(tInterval);
+	savedTime = 0;
+	difference = 0;
+	paused = 0;
+	running = 0;
+	timerDisplay.innerHTML = '';
+}
+
+function getShowTime(){
+	timerDisplay = document.getElementById('timer');
+
+	updatedTime = new Date().getTime();
+	if (savedTime){
+		difference = (updatedTime - startTime) + savedTime;
+	} else {
+		difference =  updatedTime - startTime;
+	}
+
+	// var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+	var milliseconds = Math.floor((difference % (1000)) / 1);
+
+	hours = (hours < 10) ? "0" + hours : hours;
+	minutes = (minutes < 10) ? "0" + minutes : minutes;
+	seconds = (seconds < 10) ? "0" + seconds : seconds;
+	milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
+	timerDisplay.innerHTML = minutes + ':' + seconds + ':' + milliseconds;
+}
